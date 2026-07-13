871db840c278d17b096c42751c2b71c93daf759b
Eduardo Felipe <eduardo01dev@gmail.com>
2026-07-13 13:31:10 -0300

    Aplicando classes e metódos.

---
+++ src/secao_008/_02_Criando_classe_com_tres_atributos/entities/Triangle.java
@@ -0,0 +1,8 @@
+package secao_008._02_Criando_classe_com_tres_atributos.entities;
+
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+}
---
+++ src/secao_008/_03_criando_metodo_reaproveitamento_e_delegacao/entities/Triangle.java
@@ -0,0 +1,13 @@
+package secao_008._03_criando_metodo_reaproveitamento_e_delegacao.entities;
+
+public class Triangle {
+
+    public double a;
+    public double b;
+    public double c;
+
+    public double area() {
+        double p = (a + b + c) / 2.0;
+        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+    }
+}
